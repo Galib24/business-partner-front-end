54163a073d5f1332f9276536a2d2cf0d76f667d6
SQLAdmin <abuyeahia24@gmail.com>
2023-11-16 12:32:23 +0600

    initial seaper add to home

--- src/Pages/Home/Third Section/ThirdSection.jsx
+++ src/Pages/Home/Third Section/ThirdSection.jsx
@@ -11,19 +11,11 @@ const ThirdSection = () => {
     return (
 
         <>
-            {/* <div className='relative my-10 ml-10'>
-                <img className='w-2/12' src={img1} alt="" />
-                <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
-                    <h2 className='text-sm'>TAS</h2>
-                </div>
-            </div> */}
-
-
 
             <div className='container mx-auto'>
                 <Swiper
-                     
-                      slidesPerView={4}
+
+                    slidesPerView={4}
                     autoplay={{
                         delay: 2500,
                         disableOnInteraction: true,
@@ -47,96 +39,236 @@ const ThirdSection = () => {
                     }}
                 >
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
+                            </div>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
+                            </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
+                        <div className='relative my-10 ml-0 md:ml-10 '>
                             <img className='w-full' src={img1} alt="" />
                             <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
                                 <h2 className='text-sm mt-0 ml-1'>TAS</h2>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
-                            <img className='w-full' src={img1} alt="" />
-                            <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
-                                <h2 className='text-sm mt-0 ml-1'>TAS</h2>
+                            <div className=' bg-red-300 w-full text-center'>
+                                Deccan light - Estb in 2016
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className='relative my-10 ml-0 md:ml-10'>
-                            <img className='w-full' src={img1} alt="" />
-                            <div className='absolute top-0 left-0 bg-red-500 text-white p-2 curve-container-tas'>
-                                <h2 className='text-sm mt-0 ml-1'>TAS</h2>
+                            <div className='w-full my-4'>
+                                <h2 className='text-xl'>Business Sales</h2>
+                                <h2 className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod impedit cupiditate explicabo laborum quas, labore iste ullam exercitationem. Error.</h2>
                             </div>
+
+                            <div className='flex justify-between bg-green-400 w-full'>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                                <div className='w-28 text-center  bg-red-500'>
+                                    <h2 className='mt-2'>View Profile</h2>
+                                </div>
+                            </div>
+
                         </div>
                     </SwiperSlide>
+
                     {/* Add other slides similarly */}
                 </Swiper>
             </div>
+
+
         </>
 
     );
